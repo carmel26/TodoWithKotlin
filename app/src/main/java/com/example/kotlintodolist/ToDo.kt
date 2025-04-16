e11396f1ab7ef0784e185f209a0720e98a6b963e
carmel26 <nkeshipelagie@gmail.com>
2025-04-16 10:59:11 +0300

    adding toggleStrikeThrough function to bind data when there is any change on 16Apr2024 at 10h59

--- app/src/main/java/com/example/kotlintodolist/ToDo.kt
+++ app/src/main/java/com/example/kotlintodolist/ToDo.kt
@@ -2,5 +2,5 @@ package com.example.kotlintodolist
 
 class ToDo {
     val title : String? = null
-    val isCheck: Boolean = false
+    var isChecked: Boolean = false
 }
--- app/src/main/java/com/example/kotlintodolist/TodoAdapter.kt
+++ app/src/main/java/com/example/kotlintodolist/TodoAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.kotlintodolist
 
+import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlintodolist.databinding.ItemtodoBinding
 
@@ -19,10 +21,25 @@ class TodoAdapter (private val todos : MutableList<ToDo>) : RecyclerView.Adapter
         return TodoViewHolder(binding)
     }
 
+    private fun toggleStrikeThrough(tvTodoTitle : TextView, isChecked : Boolean){
+        if (isChecked){
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags or STRIKE_THRU_TEXT_FLAG
+        }else{
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+        }
+    }
+
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         val currentTod = todos[position]
-        holder.binding.tvTodoTitle.text = currentTod.title
-        holder.binding.cbDone.isChecked = currentTod.isCheck
+        val tvTodoTitle = holder.binding.tvTodoTitle
+        val cbDone = holder.binding.cbDone
+        tvTodoTitle.text = currentTod.title
+        cbDone.isChecked = currentTod.isChecked
+        toggleStrikeThrough(tvTodoTitle, currentTod.isChecked)
+        cbDone.setOnCheckedChangeListener { _, isChecked ->
+            toggleStrikeThrough(tvTodoTitle, isChecked)
+            currentTod.isChecked = !currentTod.isChecked
+        }
     }
 
     override fun getItemCount(): Int {
