2d5a46587f5464548181932f8df29f1a20496e62
carmel26 <nkeshipelagie@gmail.com>
2025-04-16 10:45:38 +0300

    working on correction of the itemTodoAdapter on 16April2025 at 10h45

--- app/src/main/java/com/example/kotlintodolist/TodoAdapter.kt
+++ app/src/main/java/com/example/kotlintodolist/TodoAdapter.kt
@@ -1,5 +1,33 @@
 package com.example.kotlintodolist
 
-class TodoAdapter (private val todos : MutableList<ToDo>) {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlintodolist.databinding.ItemtodoBinding
+
+
+class TodoAdapter (private val todos : MutableList<ToDo>) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    inner class TodoViewHolder(val binding: ItemtodoBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TodoViewHolder {
+       val binding = ItemtodoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TodoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val currentTod = todos[position]
+        holder.binding.tvTodoTitle.text = currentTod.title
+        holder.binding.cbDone.isChecked = currentTod.isCheck
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
 
 }
+
